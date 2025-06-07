eee15f6f0ab092c919f1b8391d71a8a62eb706b8
karn-cyber <karnneelanshu@gmail.com>
2025-06-07 20:56:59 +0530

    let,const and var

---
+++ Lecture1.js
@@ -0,0 +1,11 @@
+//let,const and var are the three variable types in JS
+let a=10;
+const b=20;
+var c=30;
+console.log(a);
+console.log(b);
+console.log(c);
+// let is used because variables can be changed
+// const is used because variables cannot be changed
+// var is used because variables can be changed
+//let is mostly used now because var can cause errors many a times.

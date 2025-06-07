1b225538f1e7e42119f95e4d30e775a2e1638166
karn-cyber <karnneelanshu@gmail.com>
2025-06-07 21:32:47 +0530

    datatypes added

---
+++ Datatypes.js
@@ -0,0 +1,27 @@
+"use strict" // use strict mode to treat all code as newer version  
+// alert(3+3) // we are using nodejs and not Browser hence this will give error
+console.log(3+3); 
+
+console.log("Neelanshu");
+// Always focus on readability too  
+let name="Neel"
+let age=18
+let isLoggedIn=true
+let state=null
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+
+//number
+//bigInt
+//string
+//boolean
+//null : standalone value in javascript (representation of nothing)
+//undefined : when the value is not defined
+//symbol => unique
+ 
+// these are primitive Datatypes
+ 
+//object
+console.log(typeof null )

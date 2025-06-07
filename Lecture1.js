f55675d3e276ae4949618f4bad7c394a2dfdd27b
karn-cyber <karnneelanshu@gmail.com>
2025-06-07 21:10:26 +0530

    console table added

--- Lecture1.js
+++ Lecture1.js
@@ -9,3 +9,12 @@ console.log(c);
 // const is used because variables cannot be changed
 // var is used because variables can be changed
 //let is mostly used now because var can cause errors many a times.
+/*
+Prefer not to use var because of the issue of scope inside the blocks
+*/
+accountName="Neelanshu Karn"
+accountId="2401010295"
+accountType="Savings"
+address="GTB Nagar, Prayagraj"
+pinCode=211016
+console.table([accountName,accountId,accountType,address,pinCode])

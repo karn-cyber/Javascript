6a9ff1390361ee9836eff0bcda0f1af6aeec80b7
karn-cyber <karnneelanshu@gmail.com>
2025-06-08 13:35:26 +0530

    Operator Data Added

--- ConversionOperation.js
+++ ConversionOperation.js
@@ -20,3 +20,46 @@ console.log(booleanIsLoggedIn);
 // Empty string => false
 // some data inside => true
 // 0 => false 1 => true
+
+
+// **************** Operations ****************
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+console.log(2**3); // 2 to the power of 3
+console.log(2**52);
+console.log(2%3);
+
+let str1 = "hello";
+let str2 = " Neelanshu";
+let str3 = str1+str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(3+4*5%3);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+console.log(num1, num2, num3); 
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
+console.log(++gameCounter);
+console.log(gameCounter++);
+console.log(gameCounter);
+
+//links to study
+
+// https://tc39.es/ecma262/multipage/expressions-and-operators.html
+
+//mdndocs.net/en-US/docs/Web/JavaScript/Reference/Operators

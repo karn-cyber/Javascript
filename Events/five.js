1660812330e0b6e5eb5e2c0a59236927dbe2c135
karn-cyber <karnneelanshu@gmail.com>
2025-06-23 03:13:39 +0530

    Keyboard

---
+++ Events/five.js
@@ -0,0 +1,15 @@
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = `
+        <div class='color'>
+        <table>
+        <tr>
+        <td>${e.key === " " ? "Space": e.key}</td>
+        <td>${e.keyCode}</td>
+        <td>${e.code}</td>
+        </tr>
+        </table>
+        </div>
+    `;
+});
